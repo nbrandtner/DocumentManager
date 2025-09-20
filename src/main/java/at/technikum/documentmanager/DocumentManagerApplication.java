8e371d5e8b9b88876feb1c5c70b78782e4ae14bc
nbrandtner-tgm <nbrandtner@gmx.at>
2025-09-20 13:46:16 +0200

    Initial Spring Boot Setup

---
+++ src/main/java/at/technikum/documentmanager/DocumentManagerApplication.java
@@ -0,0 +1,13 @@
+package at.technikum.documentmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocumentManagerApplication.class, args);
+    }
+
+}
